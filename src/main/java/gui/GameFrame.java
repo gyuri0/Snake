66da42a7a8925ab398c1989d183fd09056fe83d2
Jánosi György <janosi.gyorgy1@gmail.com>
2014-05-26 08:51:38 +0200

    Gui módosítása

--- src/main/java/gui/GameFrame.java
+++ src/main/java/gui/GameFrame.java
@@ -1,7 +1,16 @@
 package gui;
 
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+
 import game.Map;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
 
 
@@ -15,5 +24,55 @@ public class GameFrame extends JFrame {
 	}
 	public GameFrame(){
 		this(Map.getDefaultMap());
+	}
+	
+	class GamePanel extends JPanel{
+		Map map;
+	    BufferedImage image;
+	    
+	    Timer timer;
+	    GamePanel(Map map){
+	    	this.map=map;
+	    	
+	    	image = map.getImage();
+	    	
+	        
+	        addKeyListener(new KeyboardController(map.getSnakes().get(0),KeyEvent.VK_LEFT,KeyEvent.VK_RIGHT));
+	        addKeyListener(new KeyboardController(map.getSnakes().get(1),KeyEvent.VK_A,KeyEvent.VK_D));
+	        setFocusable(true);
+	        
+	        ActionListener listener = new ActionListener() {
+	            public void actionPerformed(ActionEvent ae) {
+	                iterate();
+	            }
+	        };
+	        timer = new Timer(10, listener);
+	        timer.start();
+	    }
+	    public void iterate(){
+	        if(!map.isEnd()){
+	        	map.update(10);
+	        	repaint();    
+	        }
+	        else{if(map.isEnd()){
+	        		if(map.getSnakes().get(0).isLive()){
+	        			JOptionPane.showMessageDialog(this, "Az első játékos nyert");
+	        			timer.stop();
+	        			GameFrame.this.dispose();
+	        		}
+	        		else{
+	        			JOptionPane.showMessageDialog(this, "A második játékos Nyert");
+	        			timer.stop();
+	        			GameFrame.this.dispose();
+	        		}
+	        	}
+	        }
+	    }
+	    public void paintComponent(Graphics g){
+	        super.paintComponent(g);
+	        g.drawImage(image,0,0,null);
+	    }
+
+	    
 	}
 }
